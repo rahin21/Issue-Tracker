27218991ee5d5c394f930b210a7246fa6768465b
rahin21 <mdrahinzaman21@gmail.com>
2023-11-03 00:18:09 +0600

    prisma integration and data push

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = () => {
         <ul className='flex space-x-6'>
             {navItems.map(navItem => (
 
-            <Link key={navItem.href} href={navItem.href} className={currentPath === navItem.href? `text-zinc-800`:`text-zinc-500 hover:text-zinc-800 `}> {navItem.label}</Link>
+            <Link key={navItem.href} href={navItem.href} className={currentPath === navItem.href ? `text-zinc-800`:`text-zinc-500 hover:text-zinc-800 `}> {navItem.label}</Link>
             ))}
         </ul>
     </div>
