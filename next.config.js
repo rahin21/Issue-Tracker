66b2babb146352b5406f763759b9bccbcc5b1597
rahin21 <mdrahinzaman21@gmail.com>
2024-01-19 07:28:21 +0600

    cache control

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ module.exports = {
               { key: "Access-Control-Allow-Credentials", value: "false" },
               { key: "Access-Control-Allow-Origin", value: "*" },
               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                key: 'Cache-Control',value: 's-maxage=1, stale-while-revalidate=59', },
             ]
           }
         ]
