f9971817cbf2aa297d839fbf0967ebbc7dee08d8
rahin21 <mdrahinzaman21@gmail.com>
2024-01-19 07:40:35 +0600

    cache

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ module.exports = {
             // matching all API routes
             source: "/api/auth/:path*",
             headers: [
+              { key: 'Cache-Control',value: 'no-store, max-age=0, must-revalidate',},
               { key: "Access-Control-Allow-Credentials", value: "false" },
               { key: "Access-Control-Allow-Origin", value: "*" },
               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",},
-              { key: 'Cache-Control',value: 'no-store, max-age=0, must-revalidate',}
             ]
           }
         ]
