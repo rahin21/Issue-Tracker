7ec2385761e90a5ec76ffac3eaece5d7e1fcd87d
rahin21 <mdrahinzaman21@gmail.com>
2024-01-19 09:57:29 +0600

    headers

--- next.config.js
+++ next.config.js
@@ -3,17 +3,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   generateEtags: false,
-    // async headers() {
-    //     return [
-    //       {
-    //         // matching all API routes
-    //         source: "/:path*",
-    //         headers: [
-    //           // { key: 'Cache-Control',value: 'no cache, no-store, must-revalidate'},
-    //         ]
-    //       }
-    //     ]
-    //   }
+    async headers() {
+        return [
+          {
+            // matching all API routes
+            source: "/:path*",
+            headers: [
+              { key: 'Cache-Control',value: 'no cache, no-store, must-revalidate'},
+            ]
+          }
+        ]
+      }
 }
 
 
