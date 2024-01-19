d6296ab8b44e93ffe23a7cbf3eb1fdca675c19fe
rahin21 <mdrahinzaman21@gmail.com>
2024-01-19 08:38:38 +0600

    cache

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ module.exports = {
           {
             // matching all API routes
             source: "/api/auth/:path*",
+            experimental: {
+              appDir: true,
+              isrCache: false,
+            },
             headers: [
               { key: 'Cache-Control',value: 'no cache, no-store, must-revalidate'},
             ]
