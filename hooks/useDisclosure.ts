6443b5ceb5d31af5374f59513ab81f222e2790fc
rahin21 <mdrahinzaman21@gmail.com>
2025-01-13 15:14:12 +0600

    Add dynamic export for issue page and enhance data fetching with SWR revalidation options

--- hooks/useDisclosure.ts
+++ hooks/useDisclosure.ts
@@ -1,15 +1,17 @@
 "use client";
 import useSWR from "swr";
 import { issuesTypeI } from "@/types/types";
-
 // Create a fetcher function
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+// export const dynamic = "force-dynamic";
 const useDisclosure = () => {
   // Use SWR to fetch data
   const { data, error, isLoading, mutate, isValidating } = useSWR<issuesTypeI[]>(
     "/api/auth/getIssue",
-    fetcher
+    fetcher,
+    { revalidateOnFocus: true,
+      revalidateOnReconnect: true,}
   );
 
   return { 
