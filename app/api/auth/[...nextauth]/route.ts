b4cfdca78f64d3d32000da9e76934ea079d79b5a
rahin21 <mdrahinzaman21@gmail.com>
2025-01-29 08:35:41 +0600

    Move NextAuth configuration to a separate file and streamline Google provider setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
+import { authOptions } from "@/config/authOptions";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-			authorization: {
-				params: {
-					access_type: 'offline',
-					prompt: 'consent',
-					response_type: 'code',
-				},
-			},
-    }),
-    
-  ],
-};
 
 const handler = NextAuth(authOptions);
-
 
 export { handler as GET, handler as POST };
---
+++ config/authOptions.ts
@@ -0,0 +1,18 @@
+import GoogleProvider from "next-auth/providers/google";
+
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "" ,
+      authorization: {
+        params: {
+          access_type: "offline",
+          prompt: "consent",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+};
